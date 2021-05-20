ef6d7371b048b5382c4807be0fc5f28248f4e037
MaheshB77 <mbansode7696@gmail.com>
2021-05-21 02:37:01 +0530

    Multithreading using lambda expression

---
+++ src/main/java/com/example/demo/_02LambdaExpressionWithRunnable.java
@@ -0,0 +1,38 @@
+package com.example.demo;
+
+//	Without lambda expression
+//class MyThread implements Runnable {
+//
+//	@Override
+//	public void run() {
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println("My thread is running : " + i);
+//		}
+//	}
+//}
+
+public class _02LambdaExpressionWithRunnable {
+	public static void main(String[] args) {
+//		Without lambda expression
+//		MyThread myThread = new MyThread();
+//		Thread t1 = new Thread(myThread);
+//		t1.start();
+//
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println("Main thread is running : " + i);
+//		}
+
+//		With lambda expression
+		Runnable myThread = () -> {
+			for (int i = 0; i < 10; i++) {
+				System.out.println("My thread is running : " + i);
+			}
+		};
+		Thread t1 = new Thread(myThread);
+		t1.start();
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Main thread is running : " + i);
+		}
+	}
+}
