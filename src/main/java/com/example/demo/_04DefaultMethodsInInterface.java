497f0267ac5078c65c3ebcd2edfd8180d8441dcc
MaheshB77 <mbansode7696@gmail.com>
2021-05-22 18:17:49 +0530

    Default method in an interface

---
+++ src/main/java/com/example/demo/_04DefaultMethodsInInterface.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+/**
+ * @author Mahesh
+ */
+public class _04DefaultMethodsInInterface {
+	public static void main(String[] args) {
+		Demo demo = new Demo();
+		demo.m1();
+	}
+}
+
+//	Interface with 'default' method
+interface Interf {
+	default void m1() {
+		System.out.println("Default implementation");
+	}
+}
+
+class Demo implements Interf {
+
+	//	We can use default implementation of m1() or we can also override m1()
+	@Override
+	public void m1() {
+		System.out.println("Overridden implementation");
+	}
+}
