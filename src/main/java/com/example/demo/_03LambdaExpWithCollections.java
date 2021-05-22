a84c21184cec404c51b5ceadec2365dcb13f1c9e
MaheshB77 <mbansode7696@gmail.com>
2021-05-22 12:54:27 +0530

    Implemented custom sorting of custom class using lambda expression

--- src/main/java/com/example/demo/_03LambdaExpWithCollections.java
+++ src/main/java/com/example/demo/_03LambdaExpWithCollections.java
@@ -44,7 +44,7 @@ public class _03LambdaExpWithCollections {
 		list.add(8);
 		list.add(0);
 
-		list.sort((Integer a, Integer b) -> b.compareTo(a));	//	list.sort((a, b) -> b.compareTo(a));
+		list.sort((Integer a, Integer b) -> b.compareTo(a));    //	list.sort((a, b) -> b.compareTo(a));
 		System.out.println(list);
 	}
 }
@@ -86,5 +86,65 @@ class LambdaExpWithTreeMap {
 		treeMap.put(7, "Mangal");
 
 		System.out.println(treeMap);
+	}
+}
+
+/**
+ * @author Mahesh
+ * Custom sortig of custom class using lambda expression
+ */
+class CustomClassCustomSorting {
+	public static void main(String[] args) {
+
+		ArrayList<Person> persons = new ArrayList<>();
+
+		persons.add(new Person(10, "Mahesh"));
+		persons.add(new Person(20, "Aishwarya"));
+		persons.add(new Person(5, "Shubhangi"));
+		persons.add(new Person(15, "Mangal"));
+		persons.add(new Person(40, "Pravin"));
+
+		persons.sort((person1, person2) -> person2.id.compareTo(person1.id));
+
+		System.out.println(persons);
+	}
+
+
+	//	Custom class
+	static class Person {
+		private Integer id;
+		private String name;
+
+		public Person() {
+		}
+
+		public Person(Integer id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public Integer getId() {
+			return id;
+		}
+
+		public void setId(Integer id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public String toString() {
+			return "Person{" +
+					"id=" + id +
+					", name='" + name + '\'' +
+					'}';
+		}
 	}
 }
