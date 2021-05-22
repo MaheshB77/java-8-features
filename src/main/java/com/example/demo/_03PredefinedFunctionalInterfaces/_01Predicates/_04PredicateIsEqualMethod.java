8bbf0b7cfb777836ce730bdbeed2552d774f4d33
MaheshB77 <mbansode7696@gmail.com>
2021-05-23 04:10:54 +0530

    Added example of 'Predicate.isEqual' method

---
+++ src/main/java/com/example/demo/_03PredefinedFunctionalInterfaces/_01Predicates/_04PredicateIsEqualMethod.java
@@ -0,0 +1,30 @@
+package com.example.demo._03PredefinedFunctionalInterfaces._01Predicates;
+
+import java.util.function.Predicate;
+
+/**
+ * @author Mahesh
+ */
+public class _04PredicateIsEqualMethod {
+	public static void main(String[] args) {
+//		Checking equality of two objects using 'Predicate.isEqual' method
+
+		Employee emp = new Employee("Mahesh", 25);
+		Employee emp2 = new Employee("Dinesh", 22);
+		Employee emp3 = new Employee("Mahesh", 25);
+
+		Predicate<Employee> isMahesh = Predicate.isEqual(emp);
+		System.out.println(isMahesh.test(emp2));
+		System.out.println(isMahesh.test(emp3));
+	}
+}
+
+class Employee {
+	String name;
+	int age;
+
+	public Employee(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
