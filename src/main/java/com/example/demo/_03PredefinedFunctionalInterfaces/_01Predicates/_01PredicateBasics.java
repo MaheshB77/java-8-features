e4559ac5dce491f7f1fef27912ebd0b35898b107
MaheshB77 <mbansode7696@gmail.com>
2021-05-23 01:20:20 +0530

    Added more examples to predicate basics

--- src/main/java/com/example/demo/_03PredefinedFunctionalInterfaces/_01Predicates/_01PredicateBasics.java
+++ src/main/java/com/example/demo/_03PredefinedFunctionalInterfaces/_01Predicates/_01PredicateBasics.java
@@ -1,5 +1,9 @@
 package com.example.demo._03PredefinedFunctionalInterfaces._01Predicates;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.function.Predicate;
 
 
@@ -15,10 +19,21 @@ import java.util.function.Predicate;
  */
 public class _01PredicateBasics {
 	public static void main(String[] args) {
+//		Predicate to check given number is even or not
 		Predicate<Integer> isEven = (num) -> (num % 2 == 0) ? true : false;
-
 		System.out.println(isEven.test(11));
 		System.out.println(isEven.test(30));
-//		System.out.println(isEven.test("Mahesh"));	//	CE
+
+//		Predicate to check length of given string is greater than 5 or not
+		Predicate<String> isGreater = (str) -> str.length() > 5 ? true : false;
+		System.out.println(isGreater.test("Mahesh Bansode"));
+		System.out.println(isGreater.test("Java"));
+
+//		Predicate to check if given collection is empty or not
+		Predicate<Collection> collection = (coll) -> coll.isEmpty() ? true : false;
+		List<Integer> list = Arrays.asList();
+		List<Integer> list1 = Arrays.asList(1, 4);
+		System.out.println(collection.test(list));
+		System.out.println(collection.test(list1));
 	}
 }
