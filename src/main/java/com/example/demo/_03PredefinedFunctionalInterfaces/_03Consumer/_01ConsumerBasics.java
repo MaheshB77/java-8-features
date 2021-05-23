cf5d9e40ebf6c0e8eba943864a50e6613f78cc1f
MaheshB77 <mbansode7696@gmail.com>
2021-05-23 21:36:25 +0530

    Added 'Consumer' functional interface basics

---
+++ src/main/java/com/example/demo/_03PredefinedFunctionalInterfaces/_03Consumer/_01ConsumerBasics.java
@@ -0,0 +1,27 @@
+package com.example.demo._03PredefinedFunctionalInterfaces._03Consumer;
+
+/**
+ * Notes :
+ * 1. 'Consumer' contains abstract methos called 'accept(T t)'
+ * 2. It does not return anything
+ * 3. It only accepts data and process that data
+ */
+
+import java.util.function.Consumer;
+
+/**
+ * @author Mahesh
+ */
+public class _01ConsumerBasics {
+	public static void main(String[] args) {
+		/**
+		 * Problem :
+		 * Create a consumer which accepts a string and simply prints it
+		 */
+
+		Consumer<String> printer = (str) -> System.out.println(str);
+
+		printer.accept("Mahesh Bansode");
+		printer.accept("Java Kotlin Python TypeScript");
+	}
+}
