a93340ad1bc408b88ef05e0ac1863f37d7d377a9
MaheshB77 <mbansode7696@gmail.com>
2021-05-26 02:41:18 +0530

    Added 'LocalDateTime'

--- src/main/java/com/example/demo/_08DateAndTime/_01LocalDateBasics.java
+++ src/main/java/com/example/demo/_08DateAndTime/_01LocalDateBasics.java
@@ -1,6 +1,7 @@
 package com.example.demo._08DateAndTime;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 /**
@@ -8,15 +9,36 @@ import java.time.LocalTime;
  */
 public class _01LocalDateBasics {
 	public static void main(String[] args) {
+//		Date
 		LocalDate date = LocalDate.now();
+		System.out.println("Date : " + date);
 		System.out.println("Day : " + date.getDayOfMonth());
 		System.out.println("Month : " + date.getMonthValue());
 		System.out.println("Year : " + date.getYear());
 
+		System.out.println("--------------------");
+
+//		Time
 		LocalTime time = LocalTime.now();
+		System.out.println("Time : " + time);
 		System.out.println("Hour : " + time.getHour());
 		System.out.println("Minute : " + time.getMinute());
 		System.out.println("Seconds : " + time.getSecond());
 		System.out.println("Nano sec : " + time.getNano());
+
+		System.out.println("--------------------");
+
+//		DateTime (We have all methods from 'LocalDate' & 'LocalTime' in the 'LocalDateTime')
+		LocalDateTime dateTime = LocalDateTime.now();
+		System.out.println("DateTime : " + dateTime);
+		System.out.println("Date : " + dateTime.toLocalDate());
+		System.out.println("Time : " + dateTime.toLocalTime());
+		System.out.println("Day : " + dateTime.getDayOfMonth());
+		System.out.println("Month : " + dateTime.getMonthValue());
+		System.out.println("Year : " + dateTime.getYear());
+		System.out.println("Hour : " + dateTime.getHour());
+		System.out.println("Minute : " + dateTime.getMinute());
+		System.out.println("Seconds : " + dateTime.getSecond());
+		System.out.println("Nano sec : " + dateTime.getNano());
 	}
 }
