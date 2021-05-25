6c65a8d8f7cfed2082b97842d178524e7159cc1e
MaheshB77 <mbansode7696@gmail.com>
2021-05-26 03:20:43 +0530

    Added 'LocalDateTime.of()' method

--- src/main/java/com/example/demo/_08DateAndTime/_01LocalDateBasics.java
+++ src/main/java/com/example/demo/_08DateAndTime/_01LocalDateBasics.java
@@ -9,7 +9,7 @@ import java.time.LocalTime;
  */
 public class _01LocalDateBasics {
 	public static void main(String[] args) {
-//		Date
+//		LocalDate
 		LocalDate date = LocalDate.now();
 		System.out.println("Date : " + date);
 		System.out.println("Day : " + date.getDayOfMonth());
@@ -18,7 +18,7 @@ public class _01LocalDateBasics {
 
 		System.out.println("--------------------");
 
-//		Time
+//		LocalTime
 		LocalTime time = LocalTime.now();
 		System.out.println("Time : " + time);
 		System.out.println("Hour : " + time.getHour());
@@ -28,7 +28,7 @@ public class _01LocalDateBasics {
 
 		System.out.println("--------------------");
 
-//		DateTime (We have all methods from 'LocalDate' & 'LocalTime' in the 'LocalDateTime')
+//		LocalDateTime (We have all methods from 'LocalDate' & 'LocalTime' in the 'LocalDateTime')
 		LocalDateTime dateTime = LocalDateTime.now();
 		System.out.println("DateTime : " + dateTime);
 		System.out.println("Date : " + dateTime.toLocalDate());
@@ -40,5 +40,21 @@ public class _01LocalDateBasics {
 		System.out.println("Minute : " + dateTime.getMinute());
 		System.out.println("Seconds : " + dateTime.getSecond());
 		System.out.println("Nano sec : " + dateTime.getNano());
+
+		System.out.println("--------------------");
+
+		/**
+		 * Topic : LocalDateTime from given parameters
+		 * We have 'LocalDateTime.of()' method, using which we can create 'LocalDateTime' object
+		 * 'LocalDateTime.of()' is overloaded method with several options
+		 * 'LocalDateTime.of(yyyy, mm, dd, hh, ss, nn)' these are the options for overloading
+		 */
+
+		LocalDateTime dt = LocalDateTime.of(1996, 6, 7, 0, 0);
+		System.out.println("My birthday : " + dt);
+		System.out.println("After 6 months : " + dt.plusMonths(6));
+		System.out.println("Before 3 months : " + dt.minusMonths(3));
+		System.out.println("After 15 days : " + dt.plusDays(15));
+		System.out.println("Before 4 hours : " + dt.minusHours(4));
 	}
 }
